d004fa0ae335ab2b8889085d5014da12261508d6
Paulo Henrique Cuchi <paulo.cuchi@magrathealabs.com>
2022-01-03 16:14:00 -0300

    chore: add custom rubocop config

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :development do
   gem 'listen', '~> 3.3'
   gem 'rack-mini-profiler', '~> 2.0'
   gem 'rubocop', require: false
+  gem 'rubocop-rails', require: false
   gem 'spring'
   gem 'web-console', '>= 4.1.0'
 end
--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -3,7 +3,7 @@
 module Mutations
   class BaseMutation < GraphQL::Schema::Resolver
     def check_authentication
-      raise GraphQL::ExecutionError, 'Unauthenticated' unless @context[:current_user].present?
+      raise GraphQL::ExecutionError, 'Unauthenticated' if @context[:current_user].blank?
     end
   end
 end
